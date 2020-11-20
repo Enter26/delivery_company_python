ccdd8937cc0a04f0a6a300fd5c2c8c72d9d8538e
Enter26 <tylecadrian@gmail.com>
2020-11-20 22:55:04 +0100

    Create and register Vahicle model and Vehicle_type model

--- order/admin.py
+++ order/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Order, Parcel_size, Parcel_weight, Delivery_status
+from .models import Order, Parcel_size, Parcel_weight, Delivery_status, Vehicle, Vehicle_type
 
 admin.site.register(Order)
 admin.site.register(Parcel_size)
 admin.site.register(Parcel_weight)
 admin.site.register(Delivery_status)
+admin.site.register(Vehicle)
+admin.site.register(Vehicle_type)
--- order/models.py
+++ order/models.py
@@ -26,6 +26,14 @@ class Delivery_status(models.Model):
         """String for representing the Model object."""
         return self.status
 
+class Vehicle_type(models.Model):
+    """Model representing a vehicle type."""
+    v_type = models.CharField(max_length=50, help_text='Enter a vehicle type (e.g. bike)')
+    
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.v_type
+
 class Order(models.Model):
     """Model representig order (order for deliver package)"""
     size = models.ManyToManyField(Parcel_size, help_text='Select a parcel size')
@@ -49,3 +57,23 @@ class Order(models.Model):
         return ', '.join(Delivery_status.status for status in self.status.all()[:3])
     
     display_status.short_description = 'Status'
+
+class Vehicle(models.Model):
+    """Model representig Vehicles"""
+    v_type = models.ManyToManyField(Vehicle_type, help_text='Select a vehicle type')
+    production_year = models.IntegerField(default=2000)
+    check_date = models.DateField(null=True, blank=True)
+    brand = models.CharField(max_length=30, help_text='Enter a brand(e.g. Renault)')
+    v_model = models.CharField(max_length=50, help_text='Enter a brand(e.g. Kangoo)')
+    plate = models.CharField(max_length=20, help_text='LLE3421')
+    comment = models.TextField(max_length=1000, help_text='Enter a comment about behicle')
+        
+    def display_v_type(self):
+        """Create a string for the vehicle type. This is required to display vehicle type in Admin."""
+        return ', '.join(Vehicle_type.v_type for v_type in self.v_type.all()[:3])
+    
+    display_v_type.short_description = 'Vehicle_type'
+
+
+
+    
