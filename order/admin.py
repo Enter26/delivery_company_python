d4666d7d1733f6f3458f8b74452de4e415f338d2
Enter26 <tylecadrian@gmail.com>
2020-11-20 21:53:03 +0100

    Register Order model, Parcel_size model, Parcel_weight model, Delivery_status model, create superuser

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Order, Parcel_size, Parcel_weight, Delivery_status
+
+admin.site.register(Order)
+admin.site.register(Parcel_size)
+admin.site.register(Parcel_weight)
+admin.site.register(Delivery_status)
